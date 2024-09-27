1572a6983f2ea2e1bf010bebd8c4b9b59a55ff23
ponchisao326_ <ponchisao326@hotmail.com>
2024-09-27 14:08:23 +0200

    Modified GameMenuScreen.java with a Mixin to make the parent screen the custom one. Added an onJoin Sound for all players

--- src/main/java/victorgponce/com/autismonmod/mixin/GameMenuMixin.java
+++ src/main/java/victorgponce/com/autismonmod/mixin/GameMenuMixin.java
@@ -1,22 +1,14 @@
 package victorgponce.com.autismonmod.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.gui.screen.MessageScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
-import net.minecraft.text.Text;
-import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import victorgponce.com.autismonmod.screens.CustomMainMenu;
-import victorgponce.com.autismonmod.screens.CustomOptionsScreen;
 
-import java.util.List;
 
 import static victorgponce.com.autismonmod.client.AUTISMON_MODClient.LOGGER_CLIENT;
 
